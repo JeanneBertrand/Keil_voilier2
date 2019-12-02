613c0b03cfd7de38fcaa01cb94d33f4996ae53ef
Matthieu Deloffre <deloffre@insat.insa-toulouse.fr>
2019-12-02 09:57:23 +0100

    ça pimpe les .h

--- Services/Girouette.h
+++ Services/Girouette.h
@@ -1,4 +1,26 @@
+/* GIROUETTE.H
 
+Ne pas oublier le include "girouette.h" au début du main.
+
+Pour une mise en place rapide de la girouette,
+appelez dans l'ordre les fonctions suivantes :
+
+	initGirouette();
+	enableGirouette();
+	
+Afin de récupérer l'angle pour le renvoyer au servomoteur,
+procédez ainsi :
+	
+	int angle;
+	angle = getAngle();
+	
+La valeur angle pourra être réutilisée dans le futur.
+
+Combiner le angle = getAngle() de la girouette et le 
+setSailAngle(angle) du servo dans une boucle infinie
+est une bonne manière de procéder pour automatiser le 
+processus.																					
+																									*/
 
 #ifndef GIROUETTE_H
 #define GIROUETTE_H
@@ -6,11 +28,27 @@
 
 #include "stm32f103xb.h"
 
-static int INITIALANGLE = 0x011B;
+//Angle d'initialisation du compteur au passage à l'index.
+//Cette valeur a été trouvée expérimentalement en manipulant la girouette à la main.
+static int INITIALANGLE = 0x0168-0x0040;
+
+//Fonction d'initialisation de la girouette. 
+//On y trouve la définition de la structure de l'encodeur, et l'activation du Timer 3.
 void initGirouette(void);
+
+//Fonction d'activation de la girouette.
+//Elle contient uniquement la fonction EnableCounter pour le Timer 3.
 void enableGirouette(void);
+
+//Fonction de désactivation de la girouette.
+//Une fois appelée, le compteur du Timer 3 sera désactivé.
 void diableGirouette(void);
+
+//Récupération de l'angle donné par la girouette.
+//La fonction ajuste l'angle par rapport au décalage provoqué par la girouette.
 int GetAngle(void);
+
+//Initialise le compteur lorsque la girouette passe un certain point lors d'une première rotation.
 void setIndex(void);
 
 #endif
--- Services/Servo.h
+++ Services/Servo.h
@@ -1,24 +1,41 @@
-//driver for the rf receiver//
+/* SERVO.H
+
+Ne pas oublier le include "Servo.h" au début du main.
+
+Pour une mise en place rapide du servomoteur,
+appelez dans l'ordre les fonctions suivantes :
+
+	initServo();
+	enableServo();
+	
+Afin d'envoyer l'angle récupéré de la girouette
+pour l'envoyer au servomoteur, procédez ainsi :
+	
+	setSailAngle(angle);
+	
+Combiner le angle = getAngle() de la girouette et le 
+setSailAngle(angle) du servo dans une boucle infinie
+est une bonne manière de procéder pour automatiser le 
+processus.																					
+																									*/
+																									
 #ifndef SERVO_MOTEUR_H
 
 #define SERVO_MOTEUR_H
 
-/*
-
-@brief : config TIMER1
-
-@param : none
-
-@return : none
-
-*/
-
+//Fonction d'initialisation du servomoteur. 
+//On y trouve la définition de la structure du GPIO, du Timer 1 et du BDTR pour le MOE.
 void initServo(void);
 
+//Fonction d'activation du servomoteur.
+//Elle contient uniquement la fonction EnableCounter pour le Timer 1.
 void enableServo(void);
 
+//Fonction de désactivation du servomoteur.
+//Une fois appelée, le compteur du Timer 1 sera désactivé.
 void disableServo(void);
 
+//Envoi d'une valeur (comprise entre 50 = min et 100 = max) au servomoteur.
 void setSailAngle (int angle) ;
 
 #endif
